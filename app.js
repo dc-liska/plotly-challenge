687d30a15bb75c85031b71342dc1ea58751371a1
David Liska <liskadavid@gmail.com>
2019-10-22 13:43:58 -0500

    Updated requirements.txt to avoid security vulnerabilities.

---
+++ app.js
@@ -0,0 +1,80 @@
+function buildMetadata(sample) {
+
+  d3.json(`/metadata/${sample}`).then((data)=>{
+  // Use `d3.json` to fetch the metadata for a sample
+    // Use d3 to select the panel with id of `#sample-metadata`
+    var PANEL = d3.select("#sample-metadata");
+    // Use `.html("") to clear any existing metadata
+    PANEL.html("");
+
+    // Use `Object.entries` to add each key and value pair to the panel
+    Object.entries(data).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key}: ${value}`);
+      console.log(key, value);
+    }); //Terminate Object.entries
+  });
+} //Terminate buildMetadata function
+
+function buildCharts(sample) {
+
+  d3.json(`/samples/${sample}`).then((data) => {
+    const otu_ids = data.otu_ids;
+    const otu_labels = data.otu_labels;
+    const sample_values = data.sample_values;
+    console.log(otu_ids, otu_labels, sample_values);
+
+    // Bubble Chart
+    var bubbleChartLayout = {
+      hovermode: "closest",
+      xaxis: {title: "OTU ID"}
+    };
+
+    var bubbleChartData = [
+      {
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: "markers",
+        marker: {
+          size: sample_values,
+          color: otu_ids
+        }
+      }
+    ]
+
+    plotly.plot("bubble",bubbleChartData,bubbleChartLayout);
+
+    // @TODO: Build a Pie Chart
+    // HINT: You will need to use slice() to grab the top 10 sample_values,
+    // otu_ids, and labels (10 each).
+  });
+} //Terminate buildCharts function
+
+function init() {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+
+  // Use the list of sample names to populate the select options
+  d3.json("/names").then((sampleNames) => {
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+
+    // Use the first sample from the list to build the initial plots
+    const firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+}
+
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  buildCharts(newSample);
+  buildMetadata(newSample);
+}
+
+// Initialize the dashboard
+init();
